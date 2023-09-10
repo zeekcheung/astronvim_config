b870aabc2a72770e8808db2d77037781749f9ad1
zeekcheung <zeekcheung@gmail.com>
2023-09-10 15:02:03 +0800

    feat: add support for dropbar.nvim

---
+++ plugins/dropbar.lua
@@ -0,0 +1,14 @@
+return {
+  { "Bekaboo/dropbar.nvim", event = "UIEnter", opts = {} },
+  {
+    "rebelot/heirline.nvim",
+    optional = true,
+    opts = function(_, opts) opts.winbar = nil end,
+  },
+  {
+    "catppuccin/nvim",
+    optional = true,
+
+    opts = { integrations = { dropbar = { enabled = true } } },
+  },
+}
