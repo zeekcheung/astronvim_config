69aeb7869768bc5bce3121e47719fabe9a09bc53
zeekcheung <zeekcheung@gmail.com>
2023-09-13 20:09:07 +0800

    chore: update keymaps

--- mappings.lua
+++ mappings.lua
@@ -4,40 +4,63 @@
 
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+local ression = require "resession"
+local utils = require "astronvim.utils"
+local get_icon = utils.get_icon
+
 return {
   -- normal mode
   n = {
-    -- second key is the lefthand side of the map
+    -- Disable default keymaps
+    ["<leader>pA"] = false,
+    ["<leader>pl"] = false,
+    ["<leader>pv"] = false,
+    ["<leader>lS"] = false,
+    ["<leader>lh"] = false,
+    ["<leader>S"] = false,
+    ["<leader>Sl"] = false,
+    ["<leader>Ss"] = false,
+    ["<leader>St"] = false,
+    ["<leader>Sd"] = false,
+    ["<leader>Sf"] = false,
+    ["<leader>S."] = false,
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    -- quick save
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+    -- select all text in file
+    ["<C-a>"] = { "ggVG", desc = "Select All" },
+    -- paste from clipboard
+    ["C-v"] = { ":+p<cr>", desc = "Paste from Clipboard" },
+    -- undo
+    ["C-z"] = { "u", desc = "Undo" },
+    -- goto line start
+    ["gs"] = { "^", desc = "Goto line start" },
+    -- clear search with <esc>
+    ["<esc>"] = { "<cmd>noh<cr><esc>", desc = "Escape and clear hlsearchA," },
 
-    -- mappings seen under group name "Buffer"
-    -- ["<leader>bD"] = {
-    --   function()
-    --     require("astronvim.utils.status").heirline.buffer_picker(
-    --       function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-    --     )
-    --   end,
-    --   desc = "Pick to close",
-    -- },
-    -- -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
+    -- AstroNvim
+    ["<leader>A"] = { name = get_icon("ActiveLSP", 1, true) .. "AstroNvim" },
+    -- version
+    ["<leader>Av"] = { "<cmd>AstroVersion", desc = "AstroVersion" },
+    -- reload
+    ["<leader>Ar"] = { "<cmd>AstroReload", desc = "AstroReload" },
+    -- update
+    ["<leader>Au"] = { "<cmd>AstroUpdate", desc = "AstroUpdate" },
+    -- changelog
+    ["<leader>Ac"] = { "<cmd>AstroChangelog", desc = "AstroChangelog" },
+    -- rollback
+    ["<leader>AR"] = { "<cmd>AstroRollback", desc = "AstroRollback" },
+
+    -- Buffers
     ["<leader>b"] = { name = "Buffers" },
     -- previous buffer
-    ["H"] = {
+    ["L"] = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
       desc = "Next buffer",
     },
     -- next buffer
-    ["L"] = {
+    ["H"] = {
       function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
       desc = "Previous buffer",
     },
@@ -50,21 +73,11 @@ return {
       end,
       desc = "Close buffer",
     },
+    -- split window
+    ["\\"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" },
+    ["|"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
 
-    -- quick save
-    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
-    -- select all text in file
-    ["<C-a>"] = { "ggVG", desc = "Select All" },
-    -- paste from clipboard
-    ["C-v"] = { ":+p<cr>", desc = "Paste from Clipboard" },
-    -- undo
-    ["C-z"] = { "u", desc = "Undo" },
-    -- goto line start
-    ["gs"] = { "^", desc = "Goto line start" },
-
-    -- clear search with <esc>
-    ["<esc>"] = { "<cmd>noh<cr><esc>", desc = "Escape and clear hlsearchA," },
-
+    -- Find
     -- find project
     ["<leader>fp"] = { "<cmd>Telescope projects<cr>", desc = "Find Projects" },
     -- find manual pages
@@ -73,9 +86,26 @@ return {
     ["<leader>fm"] = { function() require("telescope.builtin").marks() end, desc = "Find marks" },
     -- find diagnostics
     ["<leader>fd"] = { "<cmd>Telescope diagnostics severity_bound=ERROR<cr>", desc = "Find diagnostics" },
-    -- split window
-    ["\\"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" },
-    ["|"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
+
+    -- Session
+    ["<leader>s"] = { name = get_icon("Session", 1, true) .. "Session" },
+    ["<leader>sl"] = { function() ression.load "Last Session" end, desc = "Load last session" },
+    ["<leader>ss"] = { function() ression.save() end, desc = "Save this session" },
+    ["<leader>st"] = { function() ression.save_tab() end, desc = "Save this tab's session" },
+    ["<leader>sd"] = { function() ression.delete() end, desc = "Delete a session" },
+    ["<leader>sf"] = { function() ression.load() end, desc = "Load a session" },
+    ["<leader>s."] = {
+      function() require("resession").load(vim.fn.getcwd(), { dir = "dirsession" }) end,
+      desc = "Load current directory session",
+    },
+
+    -- Lsp
+    ["<leader>lo"] = { "<cmd>AerialToggle<cr>", desc = "Toggle outline" },
+
+    -- Refactor
+    ["<leader>r"] = { name = get_icon("Refresh", 1, true) .. "Refactor" },
+
+    -- UI/UX
     -- toggle neovide fullscreen
     ["<F11>"] = { function() require("user.utils.neovide").toggle_fullScreen() end, desc = "Toggle Neovide Fullscreen" },
     -- toggle Trouble
