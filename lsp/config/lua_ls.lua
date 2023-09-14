b16b19271bf859f57c81572ddbdea0d1be0322b7
zeekcheung <zeekcheung@gmail.com>
2023-09-14 23:48:19 +0800

    refactor: improve lsp config

---
+++ lsp/config/lua_ls.lua
@@ -0,0 +1,14 @@
+local lua_ls = {
+  settings = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false,
+      },
+      completion = {
+        callSnippet = "Replace",
+      },
+    },
+  },
+}
+
+return lua_ls
